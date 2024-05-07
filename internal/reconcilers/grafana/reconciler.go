88457a766a28d9800a466b672f15307f5b39d5cb
Kim Tore Jensen <kimtjen@gmail.com>
2024-05-07 12:38:33 +0200

    Grafana reconcile constructor with dependency injection

--- internal/reconcilers/grafana/reconciler.go
+++ internal/reconcilers/grafana/reconciler.go
@@ -4,24 +4,39 @@ import (
 	"context"
 	"net/http"
 	"strings"
-	"time"
 
-	_ "github.com/grafana/grafana-openapi-client-go/client"
-	"github.com/nais/api-reconcilers/internal/reconcilers"
 	"github.com/nais/api/pkg/apiclient"
 	"github.com/nais/api/pkg/protoapi"
 	"github.com/sirupsen/logrus"
+
+	grafana_accesscontrol "github.com/grafana/grafana-openapi-client-go/client/access_control"
+	grafana_serviceaccounts "github.com/grafana/grafana-openapi-client-go/client/service_accounts"
+	grafana_teams "github.com/grafana/grafana-openapi-client-go/client/teams"
+	grafana_users "github.com/grafana/grafana-openapi-client-go/client/users"
 )
 
 const (
 	grafanaReconcilerName = "grafana"
 )
 
-func New(ctx context.Context, endpoint, serviceAccountToken string) (reconcilers.Reconciler, error) {
-	return &Reconciler{
-		httpClient:          &http.Client{Timeout: 10 * time.Second},
-		endpoint:            endpoint,
-		serviceAccountToken: serviceAccountToken,
+type grafanaReconciler struct {
+	users           grafana_users.ClientService
+	teams           grafana_teams.ClientService
+	rbac            grafana_accesscontrol.ClientService
+	serviceAccounts grafana_serviceaccounts.ClientService
+}
+
+func New(
+	users grafana_users.ClientService,
+	teams grafana_teams.ClientService,
+	rbac grafana_accesscontrol.ClientService,
+	serviceAccounts grafana_serviceaccounts.ClientService,
+) (*grafanaReconciler, error) {
+	return &grafanaReconciler{
+		users:           users,
+		teams:           teams,
+		rbac:            rbac,
+		serviceAccounts: serviceAccounts,
 	}, nil
 }
 
