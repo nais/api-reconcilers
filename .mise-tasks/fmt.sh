fc74423b41c721d05799cd4c85aa41b4782a5f43
Christer Edvartsen <christer.edvartsen@nav.no>
2025-09-10 10:27:04 +0200

    build: bump actions and use matrix

--- .mise-tasks/fmt.sh
+++ .mise-tasks/fmt.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #MISE description="Format all code"
 
 set -e
--- .mise-tasks/generate.sh
+++ .mise-tasks/generate.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #MISE description="Generate mocks using mockery"
 
 set -e
